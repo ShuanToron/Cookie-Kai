c62f8c0edac188e87465a1a8ced419b0843066c3
ShuanToron <xtruongtdo2003@gmail.com>
2023-11-19 08:22:11 +0700

    Delete source

--- DemoDATN/src/main/java/com/example/cookiekai/repository/RolesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.cookiekai.repository;
-
-import com.example.cookiekai.entity.Roles;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RolesRepository extends JpaRepository<Roles, Integer> {
-}
